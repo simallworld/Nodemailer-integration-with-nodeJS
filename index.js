2fb877657c16a9578ec0dc75735be1d91449db20
Shivam Swaroop <shivamswaroop89@gmail.com>
2025-09-20 14:44:15 +0530

    Email sent with attach file from the system

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import nodemailer from "nodemailer";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const transport = nodemailer.createTransport({
   host: "sandbox.smtp.mailtrap.io",
@@ -17,12 +23,29 @@ const sendMail = () => {
     },
     to: ["abc@test.com", "123@test.com", "xyz@test.com"],
     subject: "Testing mail sent",
+    //Text mail
     // text: "Email sent with Node.js using Nodemailer!",
-    html: `
-    <h1 style="color: #36ba9b">Hi user,</h1>
-    <p style="color: #555">This email was sent using Node.js with <b>nodemailer</b></p>
-    <p style="color: #ccc">Regards, <br>Shivam Swaroop</br></p>
-    `
+    
+    //HTML format mail
+    // html: `
+    // <h1 style="color: #36ba9b">Hi user,</h1>
+    // <p style="color: #555">This email was sent using Node.js with <b>nodemailer</b></p>
+    // <p style="color: #ccc">Regards, <br>Shivam Swaroop</br></p>
+    // `
+
+    //Mail with attachments
+    text: "Email with attachments",
+      //building file in real time
+    attachments: [{
+      filename: "testFile.txt",
+      content: "Hello this is the test file, and created in real time..."
+    },
+    //File from the system
+    {
+      filename: "testFile2.jpg",
+      path: __dirname + "/attachments/Shivam2.jpg"
+    }
+  ]
   };
 
   transport.sendMail(mailOptions, (err, info) => {
